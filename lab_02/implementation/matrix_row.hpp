b9e03334e7d61df157ab86963beb9010e51f6c75
Justarone <p_perestoronin@mail.ru>
2020-05-08 22:21:29 +0300

    Finally make MatrixRow work!

--- lab_02/implementation/matrix_row.hpp
+++ lab_02/implementation/matrix_row.hpp
@@ -1,5 +1,5 @@
 template<typename T>
-Type &MatrixRow<T>::operator[](const size_t index) {
+T &MatrixRow<T>::operator[](const size_t index) {
     if (index >= _size) {
         time_t cur_time = time(NULL);
         auto local_time = localtime(&cur_time);
@@ -10,7 +10,7 @@ Type &MatrixRow<T>::operator[](const size_t index) {
 }
 
 template<typename T>
-const Type &MatrixRow<T>::operator[](const size_t index) const {
+const T &MatrixRow<T>::operator[](const size_t index) const {
     if (index >= _size) {
         time_t cur_time = time(NULL);
         auto local_time = localtime(&cur_time);
@@ -21,7 +21,7 @@ const Type &MatrixRow<T>::operator[](const size_t index) const {
 }
 
 template<typename T>
-void MatrixRow<T>::reset(const T *const data, const size_t size) {
+void MatrixRow<T>::reset(T *data, const size_t size) {
     _size = size;
     _data.reset(data);
 }
@@ -29,15 +29,15 @@ void MatrixRow<T>::reset(const T *const data, const size_t size) {
 template<typename T>
 void MatrixRow<T>::reset() {
     _size = 0;
-    _data.reset(nullptr);
+    _data.reset();
 }
 
 template<typename T>
-Type *MatrixRow<T>::getAddr() {
+T *MatrixRow<T>::getAddr() {
     return _data.get();
 }
 
 template<typename T>
-const Type * MatrixRow<T>::getAddr() const {
+const T *MatrixRow<T>::getAddr() const {
     return _data.get();
 }
--- lab_02/include/matrix_row.hpp
+++ lab_02/include/matrix_row.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include "exception.hpp"
 
 template<typename T>
 using SharedPtr = std::shared_ptr<T>;
@@ -10,13 +11,14 @@ private:
     SharedPtr<Type[]> _data = nullptr;
     size_t _size = 0;
 public:
-    MatrixRow(const Type *const data, const size_t size): _data(data), _size(size) {}
+    MatrixRow(Type *data, const size_t size): _data(data), _size(size) {}
     MatrixRow(): _data(nullptr), _size(0) {}
     Type &operator[](const size_t index);
     const Type &operator[](const size_t index) const;
-    void reset(const Type *const data, const size_t size);
+    void reset(Type *data, const size_t size);
     void reset();
     Type *getAddr();
     const Type *getAddr() const;
 };
 
+#include "../implementation/matrix_row.hpp"
