29b5bc6c98f25690eba1bf6a1b772daf07dd9601
Justarone <p_perestoronin@mail.ru>
2020-06-02 04:02:02 +0300

    Think, I am finally done with drawer and its factory

--- lab_03/include/drawer.hpp
+++ lab_03/include/drawer.hpp
@@ -7,36 +7,21 @@
 
 class BaseDrawer {
 public:
-    virtual void draw(const shared_ptr<Component> scene, shared_ptr<BaseCamera> camera) = 0;
+    virtual void drawLine(const Point2d &p1, const Point2d &p2) = 0;
     virtual void clear() = 0;
 };
 
 
 class GtkDrawer: public BaseDrawer {
     Gtk::DrawingArea &_drawingArea;
-    void _createLine(const Point2d &p1, const Point2d &p2);
 public:
     explicit GtkDrawer(Gtk::DrawingArea &area): _drawingArea(area) {}
-    virtual void draw(const shared_ptr<Component> scene, shared_ptr<BaseCamera> camera) override;
+    virtual void drawLine(const Point2d &p1, const Point2d &p2) override;
     virtual void clear() override;
 };
 
 
-void GtkDrawer::draw(shared_ptr<Component> obj, shared_ptr<BaseCamera> cam) {
-    if (obj->isComposite()) // array of models and cams
-        for (const auto &elem: scene)
-            draw(elem);
-    // ???????????????????????????????????????????????????//
-    else if (obj->isVisible()) // model
-    {
-        // proection problems
-        Point2d p1{1, 1}, p2{20, 400};
-        _createLine(p1, p2);
-    }
-}
-
-
-void GtkDrawer::_createLine(const Point2d &p1, const Point2d &p2) {
+void GtkDrawer::drawLine(const Point2d &p1, const Point2d &p2) {
     auto cr = _drawingArea.get_window()->create_cairo_context();
     cr->move_to(p1.getX(), p1.getY());
     cr->line_to(p2.getX(), p2.getY());
--- lab_03/include/drawing_factory.hpp
+++ lab_03/include/drawing_factory.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include "drawer.hpp"
+
 class BaseDrawingFactory {
 public:
     virtual shared_ptr<BaseDrawer> createDrawer() = 0;
 };
 
+
 class GtkDrawingFactory: public BaseDrawingFactory {
-    Gtk::DrawingArea da;
+    Gtk::DrawingArea &_da;
+    shared_ptr<BaseDrawer> _drawer;
 public:
+    explicit GtkDrawingFactory(Gtk::DrawingArea &da): _da(da), _drawer(nullptr) {}
     virtual shared_ptr<BaseDrawer> createDrawer() override;
 };
+
+shared_ptr<BaseDrawer> GtkDrawingFactory::createDrawer() {
+    if (_drawer)
+        return _drawer;
+    _drawer.reset(new GtkDrawer(_da));
+    return _drawer;
+}
