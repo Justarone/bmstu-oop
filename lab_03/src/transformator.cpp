2db8732197d89a28f2690b540ea3c4da69e21985
Justarone <p_perestoronin@mail.ru>
2020-06-04 03:56:12 +0300

    Remove debug print

--- lab_03/src/transformator.cpp
+++ lab_03/src/transformator.cpp
@@ -3,21 +3,7 @@
 
 Point MatrixTransformator::transform(const Point &p) {
     MathVec<4, double> resVec = { p.getX(), p.getY(), p.getZ(), 1 };
-    //std::cout << "Matrix:\n";
-    //for (size_t i = 0; i < 4; i++) {
-        //for (size_t j = 0; j < 4; j++)
-            //std::cout << _transMatrix.at(i, j) << " ";
-        //std::cout << std::endl;
-    //}
-    //std::cout << "Vector: \n";
-    //for (size_t i = 0; i < 4; i++)
-        //std::cout << resVec.at(i) << " ";
-    //std::cout << std::endl;
-
     resVec.mulLeft(_transMatrix);
-    //std::cout << "Vector after:\n";
-    //for (size_t i = 0; i < 4; i++)
-        //std::cout << resVec.at(i) << " ";
 
     return Point(resVec.at(0), resVec.at(1), resVec.at(2));
 }
@@ -28,26 +14,7 @@ void MatrixTransformator::move(double x, double y, double z) {
                                      0, 0, 1, 0,
                                      x, y, z, 1 };
 
-    std::cout << "Matrix before move:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
-    std::cout << "move matrix:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
-    
-    _transMatrix.mulRight(moveMatrix);
-    std::cout << "Matrix after move:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
+        _transMatrix.mulRight(moveMatrix);
 }
 
 void MatrixTransformator::scale(double value) {
@@ -57,23 +24,12 @@ void MatrixTransformator::scale(double value) {
                                       0, value, 0, 0,
                                       0, 0, value, 0,
                                       0, 0, 0, 1 };
-    std::cout << "Matrix before scale:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
     _transMatrix.mulRight(scaleMatrix);
-    std::cout << "Matrix after scale:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
 }
 
 void MatrixTransformator::rotate(const Direction &dir, double value) {
     Matrix<4, double> rotateMatrix;
+    std::cout << "VAlue is: " << value;
 
     if (dir == Direction::X) {
         rotateMatrix.at(0, 0) = 1;
@@ -96,19 +52,6 @@ void MatrixTransformator::rotate(const Direction &dir, double value) {
         rotateMatrix.at(1, 0) = -sin(value);
         rotateMatrix.at(1, 1) = cos(value);
     }
-    std::cout << "Matrix before rotate:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
 
     _transMatrix.mulRight(rotateMatrix);
-
-    std::cout << "Matrix after rotate:\n";
-    for (size_t i = 0; i < 4; i++) {
-        for (size_t j = 0; j < 4; j++)
-            std::cout << _transMatrix.at(i, j) << " ";
-        std::cout << std::endl;
-    }
 }
